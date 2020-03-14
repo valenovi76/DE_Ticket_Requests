ade7bd9b95d5089663bb8f6cdbd8ad1fa7d71329
valenovi76 <valentina.novara@bt.com>
2020-03-14 16:14:29 +0000

    tasks html and loop created

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import os
+from os import path
+if path.exists("env.py"):
+    import env
 from flask import Flask, request, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
-app.config["MONGO_DBNAME"] = "DE_Tickets_DB"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGODB_NAME"] = os.environ.get("MONGODB_NAME") 
+
 mongo = PyMongo(app)
 
 
 @app.route('/')
 @app.route('/get_tickets')
 def get_tickets():
-    return render_template("tickets.html", tickets=DE_Tickets_DB.Coll_Tickets.find())
+    return render_template("tickets.html", tickets=mongo.db.Coll_Tickets.find())
     
-
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
