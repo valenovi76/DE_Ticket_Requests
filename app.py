39cd75316576ceb7e7cd1b823e606ef631a0cc1f
valenovi76 <valentina.novara@bt.com>
2020-03-14 10:10:33 +0000

    new flasks connection and html loop created

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_DBNAME"] = "DE_Tickets_DB"
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello Valentina'
+@app.route('/get_tickets')
+def get_tickets():
+    return render_template("tickets.html", tickets=DE_Tickets_DB.Coll_Tickets.find())
+    
 
 
 if __name__ == '__main__':
